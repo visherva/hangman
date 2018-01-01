4714ba5c74d344f17599b7bc993351b7ae008776
jg <dbcoop1229@gmail.com>
2017-12-31 20:25:50 -0500

    My first commit

---
+++ hangman.py
@@ -0,0 +1,50 @@
+# IMPORTANT. I changed the way this example works
+# so it will fit on smaller devices. Old versions of the book have
+# a different example. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
+
+
+def hangman(word):
+    wrong = 0
+    stages = ["",
+             "________        ",
+             "|               ",
+             "|        |      ",
+             "|        0      ",
+             "|       /|\     ",
+             "|       / \     ",
+             "|               "
+              ]
+    rletters = list(word)
+    board = ["__"] * len(word)
+    win = False
+    print("Welcome to Hangman")
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "Guess a letter"
+        char = input(msg)
+        if char in rletters:
+            cind = rletters \
+                   .index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print((" ".join(board)))
+        e = wrong + 1
+        print("\n"
+              .join(stages[0: e]))
+        if "__" not in board:
+            print("You win!")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n"
+              .join(stages[0: \
+              wrong]))
+        print("You lose! It was {}."
+              .format(word))
+
+hangman("cat")
